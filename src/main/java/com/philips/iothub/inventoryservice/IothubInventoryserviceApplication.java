4774cdd0ae1a974016d1eda27d4977e2b7daf8bb
Karan Beriwal <karanberiwal96@gmail.com>
2018-12-20 12:43:08 +0530

    123

--- src/main/java/com/philips/iothub/inventoryservice/IothubInventoryserviceApplication.java
+++ src/main/java/com/philips/iothub/inventoryservice/IothubInventoryserviceApplication.java
@@ -2,7 +2,7 @@ package com.philips.iothub.inventoryservice;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = { "com.philips.iothub1223" })
+@SpringBootApplication(scanBasePackages = { "com.philips.iothub1223111221122" })
 public class IothubInventoryserviceApplication {
 
 	public static void main(String[] args) {
